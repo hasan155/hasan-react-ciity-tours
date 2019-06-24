f1f63081126e48fce6a88e53024ea4e995e5b7f3
Hasan Mahmud <hasanmahmud548589@gmail.com>
2019-06-24 07:47:42 +0600

    city tours react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import "./App.scss";
+import Navbar from "./Components/Navbar/Navbar";
+import TourList from "./Components/TourList";
+
+class App extends Component {
+  render() {
+    return (
+      <main>
+        <Navbar />
+        <TourList />
+      </main>
+    )
+  }
+}
+export default App;
